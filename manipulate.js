a43286b723d8be82b9214e7494849e7b3972c9af
Imran shah <94097713+imranshah007@users.noreply.github.com>
2022-01-24 19:56:39 +0530

    Add files via upload

---
+++ manipulate.js
@@ -0,0 +1,10 @@
+a = [2,5,8,9];
+var result =[];
+for(let i=0; i<a.length; i++){
+    var b =(a[i]+4);
+    var c =(a[i]*9);
+    result.push([b]);
+    console.log([b]);
+    result.push([c]);
+    console.log([c]);
+}
